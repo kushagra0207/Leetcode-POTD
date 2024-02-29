efbbcafab5c46db59a545a09df4ffc2553ce91a7
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2024-02-29 18:57:35 +0530

    Create LeetCode 1609.java

---
+++ LeetCode Problem OF The Day/LeetCode 1609.java
@@ -0,0 +1,60 @@
+29-02-2024
+
+  1609. Even Odd Tree
+Solved
+Medium
+Topics
+Companies
+Hint
+A binary tree is named Even-Odd if it meets the following conditions:
+
+The root of the binary tree is at level index 0, its children are at level index 1, their children are at level index 2, etc.
+For every even-indexed level, all nodes at the level have odd integer values in strictly increasing order (from left to right).
+For every odd-indexed level, all nodes at the level have even integer values in strictly decreasing order (from left to right).
+Given the root of a binary tree, return true if the binary tree is Even-Odd, otherwise return false.
+
+ 
+
+Example 1:
+
+
+  public class Solution {
+    private List<Integer> prev = new ArrayList<>();
+
+    public boolean isEvenOddTree(TreeNode root) {
+        TreeNode current = root;
+        return dfs(current, 0);
+    }
+
+    private boolean dfs(TreeNode current, int level) {
+        // Base case, an empty tree is Even-Odd
+        if (current == null) {
+            return true;
+        }
+
+        // Compare the parity of current and level
+        if (current.val % 2 == level % 2) {
+            return false;
+        }
+
+        // Add a new level to prev if we've reached a new level
+        while (prev.size() <= level) {
+            prev.add(0);
+        }
+
+        // If there are previous nodes on this level, check increasing/decreasing
+        // If on an even level, check that current's value is greater than the previous on this level
+        // If on an odd level, check that current's value is less than the previous on this level
+        if (prev.get(level) != 0 && 
+                ((level % 2 == 0 && current.val <= prev.get(level)) || 
+                 (level % 2 == 1 && current.val >= prev.get(level)))) {
+            return false;
+        }
+
+        // Add current value to prev at index level
+        prev.set(level, current.val);
+
+        // Recursively call DFS on the left and right children
+        return dfs(current.left, level + 1) && dfs(current.right, level + 1);
+    }
+}
