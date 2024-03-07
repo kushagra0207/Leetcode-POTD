5926c49d883edc01e7d922b276224e25da5c4492
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2024-03-07 07:24:16 +0530

    Create LeetCode 876.java

---
+++ LeetCode Problem OF The Day/LeetCode 876.java
@@ -0,0 +1,35 @@
+07-03-2024
+
+  876. Middle of the Linked List
+Solved
+Easy
+Topics
+Companies
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+ 876. Middle of the Linked List
+Solved
+Easy
+Topics
+Companies
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+
+
+  
+ class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+        
+    }
+}
