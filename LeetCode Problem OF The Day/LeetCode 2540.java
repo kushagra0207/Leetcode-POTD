5431cf27dbe506e4c11e766fc0836030cc0fb50e
Kushagra Gupta <97070348+kushagra0207@users.noreply.github.com>
2024-03-09 18:29:39 +0530

    Create LeetCode 2540.java

---
+++ LeetCode Problem OF The Day/LeetCode 2540.java
@@ -0,0 +1,49 @@
+09-03-2024
+
+  2540. Minimum Common Value
+Solved
+Easy
+Topics
+Companies
+Hint
+Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
+
+Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
+
+
+  public class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        // Binary search should be done on the larger array
+        // If nums1 is longer, call getCommon with the arrays swapped
+        if (nums1.length > nums2.length) {
+            return getCommon(nums2, nums1);
+        }
+        
+        // Search for each element of nums1 in nums2
+        // Return the first common element found
+        for (int num : nums1) {
+            if (binarySearch(num, nums2)) {
+                return num;
+            }
+        }
+
+        // Return -1 if there are no common elements
+        return -1;
+    }
+
+    private boolean binarySearch(int target, int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
